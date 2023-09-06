b1a3944417bd55342fc7794724961b7bd5c5d3f4
Alvaro Cuba <alvaro.cuba@tecsup.edu.pe>
2023-09-05 20:37:52 -0500

    Archivo StudentCtrl modificado por AlvaroCuba

---
+++ StudentCtrl.java
@@ -0,0 +1,22 @@
+//Le damos un nombre "Cuba"
+public class Cuba
+{
+    //Atributos de la clase
+    private String atributo1;
+    private int atributo2;
+    private float atributo3;
+
+    //Constructor con el mismo nombre de la clase
+    public Cuba(){}
+
+    //Métodos de la clase
+    public void metodo1()
+    {
+        //Método vacío
+    }
+
+    public String metodo2()
+    {
+        return "metodo2";
+    }
+}
